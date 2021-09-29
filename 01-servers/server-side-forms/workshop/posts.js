73e5e282ef0900a4641aeb5a4210728259e44157
cemalokten <cemalokten@gmail.com>
2021-09-29 10:47:35 +0100

    scope challenge

---
+++ 01-servers/server-side-forms/workshop/posts.js
@@ -0,0 +1,6 @@
+let posts = {
+  1: { body: 'something something 🏭' },
+  2: { body: 'I am online!' },
+};
+
+module.exports = posts;
--- 01-servers/server-side-forms/workshop/server.js
+++ 01-servers/server-side-forms/workshop/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const posts = require('./posts.js');
 
 const server = express();
@@ -13,7 +12,7 @@ server.get('/', (request, response) => {
     const match = post.title.toLowerCase().includes(search.toLowerCase());
 
     if (match || !search) {
-      listItems += `<li>${post.title} - ${post.body}</li>`;
+      listItems += `<li>${post.body}</li>`;
     }
   }
 
@@ -27,16 +26,44 @@ server.get('/', (request, response) => {
     <body>
     
       <ul>${listItems}</ul>
-
-         <form method="POST">
+      <form >
         <label id="search">Search dogs</label>
         <input id="search" type="search" name="search" placeholder="E.g. rover">
         <button>Search</button>
+      </form>
+
+    </body>
+  </html>`;
+
+  response.send(html);
+});
+
+server.get('/add-post', (request, response) => {
+  const html = `
+  <!doctype html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title>Dogs!</title>
+    </head>
+    <body>
+   <form method="POST">
+        <label id="create-post" >create-post</label>
+        <input id="create-post" type="create-post" name=body placeholder="E.g. rover">
+        <button>create-post</button>
       </form>
     </body>
   </html>`;
 
   response.send(html);
+});
+
+const bodyParser = express.urlencoded({ extended: false });
+
+server.post('/add-post', bodyParser, (request, response) => {
+  const newPost = request.body;
+  console.log(newPost);
+  console.log(posts);
 });
 
 const PORT = 3333;
